713b78d6894e712d6aa869daa5d8350754ce6a78
Aeowynn <kara.james@colorado.edu>
2015-03-10 22:33:51 -0600

    Started work on Setup_Players

--- SmallworldScore.py
+++ SmallworldScore.py
@@ -2,6 +2,8 @@
 # Kara James
 # Began Spring 2014, continued implementing Spring 2015
 
+NumberPlayers = 0
+
 # Player class for keeping track of a player's score
 class Player:
 	PlayerName = "Unknown"
@@ -33,12 +35,26 @@ def Welcome():
 	# Ask amount of players
 	amount_response = raw_input("First off, how many players are in this game? ")
 	if int(amount_response) < 6 and 1 < int(amount_response):
-		number = amount_response
-		print "\nNumber of players: ", number, "\n"
+		num = int(amount_response)
+		print "\nNumber of players: ", num, "\n"
 	else:
 		print "Error! Not a legal amount of players!"
+	return num
 
-
+# Make classes for each player
+def Setup_Players(num):
+	temp = 1
+	while num > 1:
+		print "Hello!"
+		temp = temp + 1
+		print "Now making player", temp
+		num = num - 1
+		print "Num:", num
+	print "Game is set up for", temp, "players!"
 
 # Calling Functions
-Welcome()
+# Set value for number of players
+NumberPlayers = Welcome()
+Setup_Players(NumberPlayers)
+
+print "Outside Num:", NumberPlayers
